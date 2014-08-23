107048768919d864c2f615dc96a05632cd5054c8
Dipak C. Gajjar <me@dipakgajjar.com>
2014-08-23 19:53:25 +0530

    Added function dcg_add_to_htaccess
    Function to add rewrite rules in .htaccess

--- wp-performance-score-booster.php
+++ wp-performance-score-booster.php
@@ -9,12 +9,55 @@ Author URI: http://www.dipakgajjar.com/
 */
 
 /* Remove query strings from static content */
-function dcg_remove_query_strings( $src ){
+function dcg_remove_query_strings( $src ) {
 	$rqs = explode( '?ver', $src );
         return $rqs[0];
 }
-
 add_filter( 'script_loader_src', 'dcg_remove_query_strings', 15, 1 );
 add_filter( 'style_loader_src', 'dcg_remove_query_strings', 15, 1 );
+
+function dcg_add_to_htaccess( $rules ) {
+
+// Add the rewrite rules in .htaccess
+$htaccess_content = <<<EOD
+\n<IfModule mod_deflate.c>
+## Added by WP Performance Score Booster ##
+## BEGIN : Enable GZIP Compression (compress text, html, javascript, css, xml and so on) ##
+AddOutputFilterByType DEFLATE text/plain
+AddOutputFilterByType DEFLATE text/html
+AddOutputFilterByType DEFLATE text/xml
+AddOutputFilterByType DEFLATE text/css
+AddOutputFilterByType DEFLATE application/xml
+AddOutputFilterByType DEFLATE application/xhtml+xml
+AddOutputFilterByType DEFLATE application/rss+xml
+AddOutputFilterByType DEFLATE application/javascript
+AddOutputFilterByType DEFLATE application/x-javascript
+AddOutputFilterByType DEFLATE application/x-httpd-php
+AddOutputFilterByType DEFLATE application/x-httpd-fastphp
+AddOutputFilterByType DEFLATE image/svg+xml
+SetOutputFilter DEFLATE
+</IfModule>
+## END : Enable GZIP Compression ##
+
+## Added by WP Performance Score Booster ##
+## BEGIN : Expires Caching (Leverage Browser Caching) ##
+<IfModule mod_expires.c>
+ExpiresActive On
+ExpiresByType image/jpg "access 2 week"
+ExpiresByType image/jpeg "access 2 week"
+ExpiresByType image/gif "access 2 week"
+ExpiresByType image/png "access 2 week"
+ExpiresByType text/css "access 2 week"
+ExpiresByType application/pdf "access 2 week"
+ExpiresByType text/x-javascript "access 2 week"
+ExpiresByType application/x-shockwave-flash "access 2 week"
+ExpiresByType image/x-icon "access 2 week"
+ExpiresDefault "access 2 week"
+</IfModule>
+## END : Expires Caching (Leverage Browser Caching) ##\n\n
+EOD;
+    return $htaccess_content . $rules;
+}
+add_filter('mod_rewrite_rules', 'dcg_add_to_htaccess');
 
 ?>
